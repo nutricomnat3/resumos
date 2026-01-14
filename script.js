d20b8bea8de1650fb8d4aab5f8bc24b8e5b000ce
Andre Costa <andrevgcosta02@gmail.com>
2026-01-13 22:41:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let selectedResumos = [];
 let resumosData = [];
 
-console.log("NutriComNat - V.20250818.1") // Version
+console.log("NutriComNat - V.20260113.1") // Version
 
 fetch("src/data/data.json")
   .then(response => response.json())
